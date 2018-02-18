a90205c43c1052a25da8ca5e3de0ef19a20dfafa
Vivek Bhat <vbhathosmat@unomaha.edu>
2018-02-18 17:33:06 -0600

    first push

--- efsblog/settings.py
+++ efsblog/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xj3+xn#^ha!ht=qc5p4tzgksu88^g7#5j%jgx9jro66c)hts_!'
-
+#SECRET_KEY = 'xj3+xn#^ha!ht=qc5p4tzgksu88^g7#5j%jgx9jro66c)hts_!'
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
